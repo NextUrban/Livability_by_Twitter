ef0ce7ff4834d9b3f642b2474c7fd8ddff959da6
NextUrban <gsarram@memphis.edu>
2020-12-14 11:13:57 -0800

    data dl

--- Rcodes.R
+++ Rcodes.R
@@ -4,20 +4,18 @@ install.packages(“openssl”)
 install.packages('fs')
 install.packages('devtools')
 devtools::install_github("mkearney/rtweet")
+
 library(rtweet)
+library(readr)
 
 token <- create_token(app = "account",
 +                       consumer_key = " ", consumer_secret = "", access_token = " ", access_secret = " ")
  
 search_tweets(q = "#Neighborhood", lang = "en", token = token)
 
-library(rtweet)
-library(readr)
 
 tweets <- search_tweets(q = "NashTheTraffic", n = 10000, lang = "en", geocode = lookup_coords("State"))
-
 df = data.frame(lapply(tweets, as.character))
-
 write_excel_csv(x = tweets_data(df), "./StateKeywordDate.csv", na = "")
 
 
