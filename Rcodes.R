30105170a82f906fbb839bd848d3a09841bf100e
NextUrban <gsarram@memphis.edu>
2020-12-13 22:38:49 -0800

    data dl

--- Rcodes.R
+++ Rcodes.R
@@ -6,19 +6,19 @@ install.packages('devtools')
 devtools::install_github("mkearney/rtweet")
 library(rtweet)
 
-token <- create_token(app = "mypro19",
-+                       consumer_key = "IHengoMpXb8kzrW8w8QiWQA9O", consumer_secret = "PiskvnFDJxN36tHwv9dOyQUsSlIAQEpO3AnNStENIBGJTViZLu", access_token = "1108503342202785792-5sB6gM2Fk5yaibz1SWOVoUaFyJL2uq", access_secret = "d9IB6HDsqLh1LPegNwjx9JCOPlaxN8VslPbrZxNp0cGKX")
+token <- create_token(app = "account",
++                       consumer_key = " ", consumer_secret = "", access_token = " ", access_secret = " ")
  
 search_tweets(q = "#Neighborhood", lang = "en", token = token)
 
 library(rtweet)
 library(readr)
 
-tweets <- search_tweets(q = "NashTheTraffic", n = 10000, lang = "en", geocode = lookup_coords("Tennessee"))
+tweets <- search_tweets(q = "NashTheTraffic", n = 10000, lang = "en", geocode = lookup_coords("State"))
 
 df = data.frame(lapply(tweets, as.character))
 
-write_excel_csv(x = tweets_data(df), "./TNnodashNashtheTrafficdate.csv", na = "")
+write_excel_csv(x = tweets_data(df), "./StateKeywordDate.csv", na = "")
 
 
 
